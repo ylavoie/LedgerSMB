ed2664a1307dbd9fed82065fb230a787cfdda6d6
Yves Lavoie <ylavoie@yveslavoie.com>
2016-12-06 17:00:16 -0500

    Temporary Fix for code lisibility

--- sql/modules/Reconciliation.sql
+++ sql/modules/Reconciliation.sql
@@ -639,26 +639,27 @@ account.  For asset and expense accounts this is the debit balance, for others
 this is the credit balance.$$;
 
 CREATE OR REPLACE VIEW recon_payee AS
- SELECT n.name AS payee, rr.id, rr.report_id, rr.scn, rr.their_balance, rr.our_balance, rr.errorcode, rr."user", rr.clear_time, rr.insert_time, rr.trans_type, rr.post_date, rr.ledger_id, ac.voucher_id, rr.overlook, rr.cleared
-   FROM cr_report_line rr
-   LEFT JOIN acc_trans ac ON rr.ledger_id = ac.entry_id
-   LEFT JOIN gl ON ac.trans_id = gl.id
-   LEFT JOIN (( SELECT ap.id, e.name
-   FROM ap
-   JOIN entity_credit_account eca ON ap.entity_credit_account = eca.id
-   JOIN entity e ON eca.entity_id = e.id
-UNION
- SELECT ar.id, e.name
-   FROM ar
-   JOIN entity_credit_account eca ON ar.entity_credit_account = eca.id
-   JOIN entity e ON eca.entity_id = e.id)
-UNION
- SELECT gl.id, gl.description
-   FROM gl) n ON n.id = ac.trans_id;
+     SELECT n.name AS payee, rr.id, rr.report_id, rr.scn, rr.their_balance,
+            rr.our_balance, rr.errorcode, rr."user", rr.clear_time, rr.insert_time,
+            rr.trans_type, rr.post_date, rr.ledger_id, ac.voucher_id, rr.overlook, rr.cleared
+       FROM cr_report_line rr
+       LEFT JOIN acc_trans ac ON rr.ledger_id = ac.entry_id
+       LEFT JOIN gl ON ac.trans_id = gl.id
+       LEFT JOIN (
+            SELECT aa.id, e.name FROM (
+                    SELECT id, entity_credit_account AS eca FROM ap
+              UNION SELECT id, entity_credit_account AS eca FROM ar
+            ) aa
+            JOIN entity_credit_account eca ON aa.eca = eca.id
+            JOIN entity e ON eca.entity_id = e.id
+        UNION
+            SELECT gl.id, gl.description FROM gl
+        ) n ON n.id = ac.trans_id;
 
 CREATE OR REPLACE FUNCTION reconciliation__report_details_payee (in_report_id INT) RETURNS setof recon_payee as $$
-                select * from recon_payee where report_id = in_report_id
-                order by scn, post_date
+        SELECT * FROM recon_payee
+        WHERE report_id = in_report_id
+        ORDER BY scn, post_date
 $$ language 'sql';
 
 DROP TYPE IF EXISTS recon_payee_days CASCADE;
