be0253d7c678a188e1671e0d61507be88b09b9cf
Yves Lavoie <ylavoie@yveslavoie.com>
2021-11-28 14:56:52 -0500

    Fix linter errors

--- UI/js-src/lsmb/main-vue.js
+++ UI/js-src/lsmb/main-vue.js
@@ -1,17 +1,15 @@
 /** @format */
 
-import {createApp} from "vue";
-//import VueRouter from "vue-router";
+import { createApp } from "vue";
+// import VueRouter from "vue-router";
 
-import Home from './Home';
+import Home from "./Home";
 
-const routes = [
-//    { path: '/', component: Home }
-];
+// const routes = [
+// { path: '/', component: Home }
+// ];
 
-
-
-//export const app = new Vue({
+// export const app = new Vue({
 export const app = createApp({
-//    router
-}).mount('#main');
+    // router
+}).mount("#main");
