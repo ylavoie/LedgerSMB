40446a0a134511856b87231414cd11733b0d16fb
Yves Lavoie <ylavoie@yveslavoie.com>
2022-09-03 20:51:51 -0400

    Add jest

--- jest.config.js
+++ jest.config.js
@@ -203,7 +203,7 @@ module.exports = {
     sandboxInjectedGlobals: [],
 
     // The paths to modules that run some code to configure or set up the testing environment before each test
-    // setupFiles: [],
+    setupFiles: ["<rootDir>/tests/common/jest-init.js"],
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
     setupFilesAfterEnv: [],
@@ -217,7 +217,7 @@ module.exports = {
     snapshotSerializers: [],
 
     // The test environment that will be used for testing
-    testEnvironment: "node",
+    testEnvironment: "jsdom",
 
     // Options that will be passed to the testEnvironment
     testEnvironmentOptions: {
