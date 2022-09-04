7bc2d74ba4da40e5d5e4bc79f952d263e5565a2e
Yves Lavoie <ylavoie@yveslavoie.com>
2022-09-04 04:35:51 -0400

    Add coverage

--- jest.config.js
+++ jest.config.js
@@ -23,12 +23,12 @@ module.exports = {
     collectCoverage: false,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: [],
+    collectCoverageFrom: ["UI/**/*.{js,jsx}", "!UI/js/**/*", "!**/*.spec.js"],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: "coverage",
 
-    coveragePathIgnorePatterns: ["/node_modules/"],
+    coveragePathIgnorePatterns: ["/node_modules/", "/tests/", "/.local/"],
 
     // An array of regexp pattern strings used to skip coverage collection
     // coveragePathIgnorePatterns: ["node_modules", "<rootDir>/test/*.*"],
@@ -37,7 +37,7 @@ module.exports = {
     coverageProvider: "babel",
 
     // A list of reporter names that Jest uses when writing coverage reports
-    coverageReporters: ["json", "text", "lcov", "clover"],
+    coverageReporters: ["text", "lcov"],
 
     // An object that configures minimum threshold enforcement for coverage results
     // coverageThreshold: undefined,
