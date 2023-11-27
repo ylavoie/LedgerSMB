1924a9ed7c775866af9c9e48d228fb42b2fafd80
Yves Lavoie <ylavoie@yveslavoie.com>
2023-11-27 13:39:43 -0500

    Extend dijit durations from 200ms to 100s

---
+++ UI/js-src/lsmb/setTooltipDelay.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-param-reassign */
+
+require([
+    "dijit/Tooltip",
+    "dojo/domReady!"
+], function(Tooltip) {
+    // Set global default position for all tooltips
+    // Tooltip.defaultPosition = ["below"];
+
+    // Set global default show delay for all tooltips (in milliseconds)
+    Tooltip.showDelay = 10000; // Set the default show delay to 10000 milliseconds (100 second)
+});
